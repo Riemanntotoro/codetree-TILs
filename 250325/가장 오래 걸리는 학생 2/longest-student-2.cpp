a2daf821b37573fb7745ad0f34d98286a8e005bf
Sanghyeop Lee <geniusky24@outlook.com>
2025-03-25 23:32:46 +0900

    Title: 가장 오래 걸리는 학생 2, Time: 1079ms, Memory: 4MB, Status: Time Limit Exceed - CodeTree

--- 250325/가장 오래 걸리는 학생 2/longest-student-2.cpp
+++ 250325/가장 오래 걸리는 학생 2/longest-student-2.cpp
@@ -1,48 +1,61 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <climits>
 using namespace std;
-using ll = long long;
-static const ll INF = LLONG_MAX;
 
-int main(){
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
-
+int main() {
     int N, M;
     cin >> N >> M;
-    vector<vector<pair<int,ll>>> adj(N+1);
-    for(int i = 0; i < M; i++){
-        int u, v;
-        ll w;
-        cin >> u >> v >> w;
 
-        adj[u].push_back({v, w});
-        adj[v].push_back({u, w});
+    vector<vector<pair<int, int>>> graph(N + 1);
+    
+    for (int i = 0; i < M; i++) {
+        int u, v, d;
+        cin >> u >> v >> d;
+        graph[u].push_back({v, d});
     }
 
-    vector<ll> dist(N+1, INF);
-    dist[N] = 0;
-    priority_queue<pair<ll,int>, vector<pair<ll,int>>, greater<pair<ll,int>>> pq;
-    pq.push({0, N});
-
-    while(!pq.empty()){
-        auto [cd, u] = pq.top();
-        pq.pop();
-        if(cd > dist[u]) continue;
-        for(auto &edge : adj[u]){
-            int v = edge.first;
-            ll w = edge.second;
-            ll nd = cd + w;
-            if(dist[v] > nd){
-                dist[v] = nd;
-                pq.push({nd, v});
+    vector<int> max_time_to_reach(N + 1, 0);
+    
+    for (int start = 1; start <= N; start++) {
+        vector<int> distance(N + 1, INT_MAX);
+        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+        
+        distance[start] = 0;
+        pq.push({0, start});
+        
+        while (!pq.empty()) {
+            int dist = pq.top().first;
+            int current = pq.top().second;
+            pq.pop();
+            
+            if (dist > distance[current]) continue;
+            
+            for (auto& edge : graph[current]) {
+                int next = edge.first;
+                int next_dist = dist + edge.second;
+                
+                if (next_dist < distance[next]) {
+                    distance[next] = next_dist;
+                    pq.push({next_dist, next});
+                }
+            }
+        }
+        
+        for (int dest = 1; dest <= N; dest++) {
+            if (dest != start && distance[dest] != INT_MAX) {
+                max_time_to_reach[dest] = max(max_time_to_reach[dest], distance[dest]);
             }
         }
     }
-
-    ll ans = 0;
-    for(int i = 1; i <= N; i++){
-        ans = max(ans, dist[i]);
+    
+    int max_time = 0;
+    for (int i = 1; i <= N; i++) {
+        max_time = max(max_time, max_time_to_reach[i]);
     }
-    cout << ans << "\n";
+    
+    cout << max_time << endl;
+    
     return 0;
 }
