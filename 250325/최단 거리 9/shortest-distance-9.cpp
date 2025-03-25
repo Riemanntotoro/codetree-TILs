a02d4381825f017f48d7a824f28a90c80afe9c38
Sanghyeop Lee <geniusky24@outlook.com>
2025-03-25 23:59:51 +0900

    Title: 최단 거리 9, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 250325/최단 거리 9/shortest-distance-9.cpp
+++ 250325/최단 거리 9/shortest-distance-9.cpp
@@ -1,65 +1,61 @@
-#include <iostream>
-#include <vector>
-#include <queue>
-#include <climits>
+#include <bits/stdc++.h>
 using namespace std;
 
+const int INF = 1e9;
+
 int main() {
-    ios_base::sync_with_stdio(false);
+    ios::sync_with_stdio(false);
     cin.tie(nullptr);
-    
-    int n, m;
-    cin >> n >> m;
-    
-    vector<vector<pair<int, int>>> graph(n + 1);
-    
-    for (int i = 0; i < m; i++) {
-        int from, to, weight;
-        cin >> from >> to >> weight;
-        graph[from].push_back({to, weight});
+
+    int N, M;
+    cin >> N >> M;
+
+    vector<vector<pair<int,int>>> adj(N+1);
+    for(int i=0; i<M; i++){
+        int u,v,w;
+        cin>>u>>v>>w;
+        adj[u].push_back({v,w});
+        adj[v].push_back({u,w});
     }
-    
-    int A, B;
-    cin >> A >> B;
-    
-    vector<int> dist(n + 1, INT_MAX);
-    vector<int> prev(n + 1, -1);
-    
-    priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
-    
+
+    int A,B;
+    cin>>A>>B;
+
+    vector<int> dist(N+1, INF);
+    vector<int> parent(N+1, -1);
+    priority_queue<pair<int,int>, vector<pair<int,int>>, greater<>> pq;
+
     dist[A] = 0;
-    pq.push({0, A});
-    
-    while (!pq.empty()) {
-        int cost = pq.top().first;
-        int here = pq.top().second;
+    pq.push({0,A});
+
+    while(!pq.empty()){
+        auto [cost, cur] = pq.top();
         pq.pop();
-        
-        if (cost > dist[here]) continue;
-        
-        for (auto& edge : graph[here]) {
-            int there = edge.first;
-            int nextDist = cost + edge.second;
-            
-            if (nextDist < dist[there]) {
-                dist[there] = nextDist;
-                prev[there] = here; 
-                pq.push({nextDist, there});
+
+        if(dist[cur] < cost) continue;
+
+        for(auto nxt : adj[cur]){
+            int nextNode = nxt.first;
+            int nextDist = nxt.second;
+
+            if(dist[nextNode] > dist[cur]+nextDist ||
+              (dist[nextNode] == dist[cur]+nextDist && cur < parent[nextNode])){
+                dist[nextNode] = dist[cur]+nextDist;
+                parent[nextNode] = cur;
+                pq.push({dist[nextNode], nextNode});
             }
         }
     }
-    
-    cout << dist[B] << "\n";
-    
+
     vector<int> path;
-    for (int at = B; at != -1; at = prev[at]) {
-        path.push_back(at);
-    }
-    
-    for (int i = path.size() - 1; i >= 0; i--) {
-        cout << path[i] << " ";
-    }
+    for(int node=B; node!=-1; node=parent[node])
+        path.push_back(node);
+    reverse(path.begin(), path.end());
+
+    cout << dist[B] << "\n";
+    for(auto node : path)
+        cout << node << " ";
     cout << "\n";
-    
+
     return 0;
 }
