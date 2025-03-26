108c59b9196b7a4a68f20e6cd3ed5c4980538e62
Sanghyeop Lee <geniusky24@outlook.com>
2025-03-26 22:23:09 +0900

    Title: 천 개의 정거장, Time: 6ms, Memory: 0MB, Status: Wrong Answer - Codetree

--- 250326/천 개의 정거장/thousand-stops.cpp
+++ 250326/천 개의 정거장/thousand-stops.cpp
@@ -1,14 +1,13 @@
 #include <bits/stdc++.h>
 using namespace std;
-using pii = pair<int, int>;
-using tiii = tuple<int, int, int, int>; 
+using tiii = tuple<int, int, int, int>;
 
 const int INF = 1e9;
 
 int A, B, N;
 int bus_fare[1001];
-vector<int> bus_stops[1001];   
-vector<int> buses_by_stop[1001];  
+vector<int> bus_stops[1001];
+vector<int> buses_by_stop[1001];
 int dist[1001], time_taken[1001];
 
 int main() {
@@ -17,28 +16,27 @@ int main() {
 
     cin >> A >> B >> N;
 
-    for (int i = 0; i < N; i++) {
+    for (int i = 0; i < N; ++i) {
         int cnt;
         cin >> bus_fare[i] >> cnt;
         bus_stops[i].resize(cnt);
-        for (int j = 0; j < cnt; j++) {
+        for (int j = 0; j < cnt; ++j) {
             cin >> bus_stops[i][j];
             buses_by_stop[bus_stops[i][j]].push_back(i);
         }
     }
 
-    for (int i = 1; i <= 1000; i++) {
+    for (int i = 1; i <= 1000; ++i) {
         dist[i] = INF;
         time_taken[i] = INF;
     }
 
     priority_queue<tiii, vector<tiii>, greater<>> pq;
 
-    for (int i = 0; i < buses_by_stop[A].size(); i++) {
-        int bus = buses_by_stop[A][i];
-        for (int j = 0; j < bus_stops[bus].size(); j++) {
-            if (bus_stops[bus][j] == A) {
-                pq.push({bus_fare[bus], 0, bus, j});
+    for (int bus : buses_by_stop[A]) {
+        for (int idx = 0; idx < bus_stops[bus].size(); ++idx) {
+            if (bus_stops[bus][idx] == A) {
+                pq.push({bus_fare[bus], 0, bus, idx});
             }
         }
     }
@@ -53,13 +51,15 @@ int main() {
         dist[stop] = cost;
         time_taken[stop] = t;
 
-        if (idx + 1 < bus_stops[bus].size()) {
-            pq.push({cost, t + 1, bus, idx + 1});
+        for (int i = idx + 1; i < bus_stops[bus].size(); ++i) {
+            int next_stop = bus_stops[bus][i];
+            int next_time = t + (i - idx);
+            pq.push({cost, next_time, bus, i});
         }
 
         for (int new_bus : buses_by_stop[stop]) {
             if (new_bus == bus) continue;
-            for (int i = 0; i < bus_stops[new_bus].size(); i++) {
+            for (int i = 0; i < bus_stops[new_bus].size(); ++i) {
                 if (bus_stops[new_bus][i] == stop) {
                     pq.push({cost + bus_fare[new_bus], t, new_bus, i});
                 }
@@ -67,10 +67,11 @@ int main() {
         }
     }
 
-    if (dist[B] == INF)
+    if (dist[B] == INF) {
         cout << "-1 -1\n";
-    else
-        cout << dist[B] << " " << time_taken[B] << "\n";
+    } else {
+        cout << dist[B] << " " << time_taken[B] << '\n';
+    }
 
     return 0;
 }
