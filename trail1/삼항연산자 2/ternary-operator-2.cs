4fee354e763d100494c29e0b7b900b5732d3b9c9
Sanghyeop Lee <geniusky24@outlook.com>
2026-05-08 12:04:13 +0900

    [Passed] 삼항연산자 2 (61ms, 5MB)

---
+++ trail1/삼항연산자 2/ternary-operator-2.cs
@@ -0,0 +1,11 @@
+using System;
+
+public class Codetree
+{  
+    public static void Main()
+    {
+        string c = Console.ReadLine();
+        char val = c=="1" ? 't' : 'f';
+        Console.WriteLine(val);
+    }
+}
