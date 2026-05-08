28f35e8080459e7c319124f04954d4c67e525f8a
Sanghyeop Lee <geniusky24@outlook.com>
2026-05-08 11:08:10 +0900

    [Passed] 정수의 조건 여부 (64ms, 5MB)

---
+++ trail1/정수의 조건 여부/numbers-condition.cs
@@ -0,0 +1,11 @@
+using System;
+
+public class Codetree
+{  
+    public static void Main()
+    {
+        int a = int.Parse(Console.ReadLine());
+        if (a >= 113) {Console.WriteLine(1);}
+        else {Console.WriteLine(0);}
+    }
+}
