4edfefb9ee9cbee6b526052fb1d29c56ab19c8fb
Sanghyeop Lee <geniusky24@outlook.com>
2026-05-08 11:40:32 +0900

    [Passed] 삼항연산자 (64ms, 5MB)

---
+++ trail1/삼항연산자/ternary-operator.cs
@@ -0,0 +1,11 @@
+using System;
+
+public class Codetree
+{  
+    public static void Main()
+    {
+        int a = int.Parse(Console.ReadLine());
+        string val = a==100 ? "pass" : "failure";
+        Console.WriteLine(val);
+    }
+}
