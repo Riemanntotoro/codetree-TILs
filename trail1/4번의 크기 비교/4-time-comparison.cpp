f41231a461bbad5bd8b7a3a43a7742b37d0aa63e
Sanghyeop Lee <geniusky24@outlook.com>
2026-05-11 17:04:45 +0900

    [Passed] 4번의 크기 비교 (8ms, 0MB)

---
+++ trail1/4번의 크기 비교/4-time-comparison.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int A, B, C, D, E;
+    cin >> A >> B >> C >> D >> E;
+    cout << (A > B) << "\n" << (A > C) << "\n" << (A > D) << "\n" << (A > E) << "\n";
+    return 0;
+}
