47cdc9d791313308b777d2e9ac34709c227381b5
Sanghyeop Lee <geniusky24@outlook.com>
2026-05-11 16:22:11 +0900

    [Passed] 특정 조건 두 정수 비교 (127ms, 10MB)

---
+++ trail1/특정 조건 두 정수 비교/specific-comparison-of-two-natural-numbers.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int val1 = (a < b) ? 1 : 0;
+        int val2 = (a == b) ? 1 : 0;
+        System.out.println(val1 + " " + val2);
+    }
+}
