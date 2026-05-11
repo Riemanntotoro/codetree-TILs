39b38d9ede65026e6590fa0f7bc2dc33da812d1b
Sanghyeop Lee <geniusky24@outlook.com>
2026-05-11 14:56:09 +0900

    [Passed] 정수의 조건 여부 2 (63ms, 5MB)

---
+++ trail1/정수의 조건 여부 2/numbers-condition-2.cs
@@ -0,0 +1,12 @@
+using System;
+
+public class Codetree
+{  
+    public static void Main()
+    {
+        int a = int.Parse(Console.ReadLine());
+        if (a == 5) {Console.WriteLine("A");}
+        if (a % 2 == 0) {Console.WriteLine("B");}
+        Console.WriteLine(" ");
+    }
+}
