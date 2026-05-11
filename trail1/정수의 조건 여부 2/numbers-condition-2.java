841ccbc1f11253bf1bdabb2d58491f1b1eddffdf
Sanghyeop Lee <geniusky24@outlook.com>
2026-05-11 14:31:58 +0900

    [Passed] 정수의 조건 여부 2 (129ms, 10MB)

---
+++ trail1/정수의 조건 여부 2/numbers-condition-2.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        char result = ' ';
+        if (a == 5) {result = 'A';}
+        if (a % 2 == 0) {result = 'B';}
+        System.out.println(result);
+    }
+}
