ce7c9345495bb9dd58a6054d7f06a9b249492bb2
Sanghyeop Lee <geniusky24@outlook.com>
2026-05-12 12:08:45 +0900

    [Passed] 세 수의 중간값 (125ms, 10MB)

---
+++ trail1/세 수의 중간값/median-of-three-numbers.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+        int C = sc.nextInt();
+        int val = (B > A && B < C) ? 1 : 0;
+        System.out.println(val);
+    }
+}
