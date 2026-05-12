c4df7db293331dfe5a5ef337b93fee74e9b97c44
Sanghyeop Lee <geniusky24@outlook.com>
2026-05-12 15:52:37 +0900

    [Passed] 좀 더 어려운 수학 점수 (127ms, 10MB)

---
+++ trail1/좀 더 어려운 수학 점수/math-scores-are-more-difficult.java
@@ -0,0 +1,14 @@
+import java.util.Scanner; 
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int Am = sc.nextInt();
+        int Ae = sc.nextInt();
+        int Bm = sc.nextInt();
+        int Be = sc.nextInt();
+        if (Am == Bm && Ae > Be) {System.out.println("A");}
+        else if (Am == Bm && Ae < Be) {System.out.println("B");}
+        else if (Am > Bm) {System.out.println("A");}
+        else {System.out.println("B");}
+    }
+}
