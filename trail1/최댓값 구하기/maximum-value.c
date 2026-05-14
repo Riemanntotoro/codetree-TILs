cb7b6102ba8947741466eda4616c647c99e578be
Sanghyeop Lee <geniusky24@outlook.com>
2026-05-14 13:40:51 +0900

    [Passed] 최댓값 구하기 (8ms, 0MB)

---
+++ trail1/최댓값 구하기/maximum-value.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    int a, b, c;
+    scanf("%d %d %d", &a, &b, &c);
+    int val = a;
+    if (b > val) {val = b;}
+    if (c > val) {val = c;}
+    printf("%d", val);
+    return 0;
+}
