50e4dc81aff6665cc5a9ef764e0f09847a4c97fb
Sanghyeop Lee <geniusky24@outlook.com>
2026-05-14 16:21:27 +0900

    [Passed] 정수 입력받아 배수 출력 (64ms, 6MB)

---
+++ trail1/정수 입력받아 배수 출력/print-multiple-of-input.cs
@@ -0,0 +1,10 @@
+using System;
+
+public class Codetree
+{  
+    public static void Main()
+    {
+        int N = int.Parse(Console.ReadLine());
+        for(int i=1; i<=5; i++) {Console.Write(N*i + " ");}
+    }
+}
