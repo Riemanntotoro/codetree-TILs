f22c0c245980c9f6fa9a9e7e8e60e431556586b4
Sanghyeop Lee <geniusky24@outlook.com>
2026-05-15 16:37:23 +0900

    [Passed] 증가시키며 출력하기 3 (53ms, 5MB)

---
+++ trail1/증가시키며 출력하기 3/increase-and-print-3.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Linq;
+public class Codetree
+{  
+    public static void Main()
+    {
+        int i =10;
+        while(i <= 26) {
+            Console.Write(i + " ");
+            i += 2;
+        }
+    }
+}
