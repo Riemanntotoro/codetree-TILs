f030afa8dbc87d5972d6f5c84f8256c89e222ac4
Sanghyeop Lee <geniusky24@outlook.com>
2026-05-15 17:10:36 +0900

    [Passed] 감소시키며 출력하기 2 (55ms, 4MB)

---
+++ trail1/감소시키며 출력하기 2/decrease-and-print-2.cs
@@ -0,0 +1,13 @@
+using System;
+
+public class Codetree
+{  
+    public static void Main()
+    {
+        int i = 26;
+        while (i >= 10) {
+            Console.Write(i + " ");
+            i--;
+        }
+    }
+}
