66b7505f169e9ffc74b54ac84fb9ae6ce67941fa
Sanghyeop Lee <geniusky24@outlook.com>
2026-05-15 16:00:46 +0900

    [Passed] N까지 자연수 출력 (8ms, 0MB)

---
+++ trail1/N까지 자연수 출력/print-1-to-n.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int N;
+    cin >> N;
+    int i=0;
+    while (i < N) {
+        i++;
+        cout << i << " ";
+    }
+    return 0;
+}
