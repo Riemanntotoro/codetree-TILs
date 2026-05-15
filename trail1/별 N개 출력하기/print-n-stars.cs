d27b3b121ffc375988a6a1c64c1b9a67043f5928
Sanghyeop Lee <geniusky24@outlook.com>
2026-05-15 16:14:01 +0900

    [Passed] 별 N개 출력하기 (66ms, 7MB)

---
+++ trail1/별 N개 출력하기/print-n-stars.cs
@@ -0,0 +1,13 @@
+using System;
+
+public class Codetree
+{  
+    public static void Main()
+    {
+        int N = int.Parse(Console.ReadLine());
+        while (N > 0) {
+            Console.WriteLine("*");
+            N--;
+        }
+    }
+}
