90eb418b24d583186fbd1da5ea6626f9c0881ee4
Sanghyeop Lee <geniusky24@outlook.com>
2026-05-15 16:45:35 +0900

    [Passed] 3의 배수 출력하기 (8ms, 0MB)

---
+++ trail1/3의 배수 출력하기/print-multiple-of-number-three.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int N;
+    cin >> N;
+    int i=1;
+    while (i <= N) {
+        if (i % 3 == 0) {cout << i << " ";}
+        i++;
+    }
+    return 0;
+}
