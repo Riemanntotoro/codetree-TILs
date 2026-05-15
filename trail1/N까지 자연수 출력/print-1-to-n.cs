6b2de6a0c45fa8b12b1fce418f8d4bff239383a9
Sanghyeop Lee <geniusky24@outlook.com>
2026-05-15 16:00:11 +0900

    [Passed] N까지 자연수 출력 (64ms, 5MB)

---
+++ trail1/N까지 자연수 출력/print-1-to-n.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Linq;
+
+public class Codetree
+{  
+    public static void Main()
+    {
+        int N = int.Parse(Console.ReadLine());
+        int i = 0;
+        while(i < N) {
+            i++;
+            Console.Write(i + " ");
+        }
+
+    }
+}
