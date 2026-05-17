ee7f7453a9e7075c2be15817be43b5c43214fe92
Sanghyeop Lee <geniusky24@outlook.com>
2026-05-18 08:58:40 +0900

    [Passed] 문자 출력하기 (61ms, 5MB)

---
+++ trail1/문자 출력하기/print-text.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Linq;
+
+public class Codetree
+{  
+    public static void Main()
+    {
+        char c = Console.ReadLine()[0];
+        for(int i=0; i<8; i++) {
+            Console.Write(c);
+        }
+    }
+}
