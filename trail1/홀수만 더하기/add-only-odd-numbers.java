53a0bf927b00d389ae075990b4c3f9a0c4e8ae63
Sanghyeop Lee <geniusky24@outlook.com>
2026-05-18 17:50:05 +0900

    [Passed] 홀수만 더하기 (130ms, 10MB)

---
+++ trail1/홀수만 더하기/add-only-odd-numbers.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int ssum = 0;
+        for(int i=0; i<N; i++) {
+            int a= sc.nextInt();
+            if (a % 3 == 0 && a % 2 == 1) {ssum += a;}
+        }
+        System.out.println(ssum);
+    }
+}
