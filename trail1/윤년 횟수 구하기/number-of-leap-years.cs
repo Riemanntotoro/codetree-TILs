b6b321701045a6cde4c8f5b1a964383ac3311142
Sanghyeop Lee <geniusky24@outlook.com>
2026-05-18 17:12:14 +0900

    [Passed] 윤년 횟수 구하기 (66ms, 5MB)

---
+++ trail1/윤년 횟수 구하기/number-of-leap-years.cs
@@ -0,0 +1,14 @@
+using System;
+
+public class Codetree
+{  
+    public static void Main()
+    {
+        int N = int.Parse(Console.ReadLine());
+        int cnt = 0;
+        for (int i=1; i<=N; i++) {
+            if ((i % 4 == 0 && i % 100 != 0) || i % 400 == 0) {cnt++;}
+        }
+        Console.WriteLine(cnt);
+    }
+}
