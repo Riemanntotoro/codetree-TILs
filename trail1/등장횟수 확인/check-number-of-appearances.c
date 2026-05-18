700415d12727b6ae04053b87a79094e6495f6bf0
Sanghyeop Lee <geniusky24@outlook.com>
2026-05-18 17:19:19 +0900

    [Passed] 등장횟수 확인 (7ms, 0MB)

---
+++ trail1/등장횟수 확인/check-number-of-appearances.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    int cnt = 0;
+    for (int i=0; i<5; i++) {
+        int a;
+        scanf("%d", &a);
+        if (a % 2 == 0) {cnt++;}
+    }
+    printf("%d", cnt);
+    return 0;
+}
