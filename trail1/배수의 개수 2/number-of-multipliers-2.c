d4e1f7a0f8531ced796b2661bab7effd3d514722
Sanghyeop Lee <geniusky24@outlook.com>
2026-05-18 16:26:15 +0900

    [Passed] 배수의 개수 2 (8ms, 0MB)

---
+++ trail1/배수의 개수 2/number-of-multipliers-2.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    int cnt = 0;
+    for (int i=0; i<10; i++) {
+        int a;
+        scanf("%d", &a);
+        if (a % 2 == 1) {cnt++;}
+    }
+    printf("%d", cnt);
+    return 0;
+}
