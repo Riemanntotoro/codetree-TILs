b76637bd751399ac9df77e382ff3d2da199652ad
Sanghyeop Lee <geniusky24@outlook.com>
2026-05-18 14:29:00 +0900

    [Passed] a의 배수 (130ms, 10MB)

---
+++ trail1/a의 배수/multiple-of-a.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int A = sc.nextInt();
+        int i = 1;
+        while (i <= N) {
+            if (i % A == 0) {System.out.println(1);}
+            else {System.out.println(0);}
+            i++;
+        }
+    }
+}
