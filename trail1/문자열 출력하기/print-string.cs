dc6ee019303019d6c41099972f901ebf833d37a0
Sanghyeop Lee <geniusky24@outlook.com>
2026-05-18 09:15:32 +0900

    [Passed] 문자열 출력하기 (68ms, 14MB)

---
+++ trail1/문자열 출력하기/print-string.cs
@@ -0,0 +1,12 @@
+using System;
+
+public class Codetree
+{  
+    public static void Main()
+    {
+        int N = int.Parse(Console.ReadLine());
+        for (int i=0; i<N; i++) {
+            Console.WriteLine("LeebrosCode");
+        }
+    }
+}
