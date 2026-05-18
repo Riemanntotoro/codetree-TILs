1f0ea3b5daeeb0d38a58af41c7268111592572f1
Sanghyeop Lee <geniusky24@outlook.com>
2026-05-18 17:10:37 +0900

    [Passed] 윤년 횟수 구하기 (1ms, 0MB)

---
+++ trail1/윤년 횟수 구하기/number-of-leap-years.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int N;
+    cin >> N;
+    int cnt = 0;
+    for (int i=1; i<=N; i++) {
+        if ((i % 4 == 0 && i % 100 != 0) || i % 400 == 0) {cnt++;}
+    }
+    cout << cnt << endl;
+    return 0;
+}
