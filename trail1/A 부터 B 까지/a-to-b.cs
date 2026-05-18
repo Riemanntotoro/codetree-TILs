c5996dccc3bebade29786494cf363384e80b1728
Sanghyeop Lee <geniusky24@outlook.com>
2026-05-18 15:16:34 +0900

    [Passed] A 부터 B 까지 (62ms, 5MB)

---
+++ trail1/A 부터 B 까지/a-to-b.cs
@@ -0,0 +1,16 @@
+using System;
+
+public class Codetree
+{  
+    public static void Main()
+    {
+        string[] arr = Console.ReadLine().Split();
+        var(A, B) = (int.Parse(arr[0]), int.Parse(arr[1]));
+        int i = A;
+        while(i <= B) {
+            Console.Write(i + " ");
+            if (i % 2 == 1) {i *= 2;}
+            else {i += 3;}
+        }
+    }
+}
