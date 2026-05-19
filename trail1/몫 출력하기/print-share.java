56747308bbe9d439e8b99598375b93f21833f8af
Sanghyeop Lee <geniusky24@outlook.com>
2026-05-19 16:54:36 +0900

    [Passed] 몫 출력하기 (120ms, 11MB)

---
+++ trail1/몫 출력하기/print-share.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int cnt = 0;
+        while (true) {
+            int a = sc.nextInt();
+            if (a % 2 == 1) continue;
+            System.out.println(a/2);
+            cnt++;
+            if (cnt == 3) break;
+        }
+    }
+}
