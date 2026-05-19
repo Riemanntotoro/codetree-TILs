c09b6652057538ccc7f945dfa604a3298092be70
Sanghyeop Lee <geniusky24@outlook.com>
2026-05-19 17:03:20 +0900

    [Passed] 사각형의 넓이 계속 계산하기 (1ms, 0MB)

---
+++ trail1/사각형의 넓이 계속 계산하기/continue-calculating-width-of-the-rectangle.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    while (1) {
+        int a, b;
+        char c;
+        scanf("%d %d %c", &a, &b, &c);
+        printf("%d\n", a*b);
+        if (c == 'C') break;
+    }
+    return 0;
+}
