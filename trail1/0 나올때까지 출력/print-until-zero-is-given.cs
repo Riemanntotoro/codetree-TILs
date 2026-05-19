63e48f65c4bad44bb89a99b8cb04eafb994a8941
Sanghyeop Lee <geniusky24@outlook.com>
2026-05-19 14:59:31 +0900

    [Passed] 0 나올때까지 출력 (64ms, 5MB)

---
+++ trail1/0 나올때까지 출력/print-until-zero-is-given.cs
@@ -0,0 +1,13 @@
+using System;
+
+public class Codetree
+{  
+    public static void Main()
+    {
+        while(true) {
+            int a = int.Parse(Console.ReadLine());
+            if (a == 0) break;
+            Console.WriteLine(a);
+        }
+    }
+}
