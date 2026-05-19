7af4f77ef6fbb2160ccf97cda70419fd4c46e6ce
Sanghyeop Lee <geniusky24@outlook.com>
2026-05-19 16:33:28 +0900

    [Passed] 출석 부르기 2 (150ms, 10MB)

---
+++ trail1/출석 부르기 2/calling-attendance-2.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        while (true) {
+            int N = sc.nextInt();
+            if (N == 1) {System.out.println("John");}
+            else if (N == 2) {System.out.println("Tom");}
+            else if (N == 3) {System.out.println("Paul");}
+            else if (N == 4) {System.out.println("Sam");}
+            else {System.out.println("Vacancy"); break;}
+        }
+    }
+}
