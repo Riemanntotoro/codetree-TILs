de3f01261cdb60512a52adc471c70c05ccff0c93
Sanghyeop Lee <geniusky24@outlook.com>
2026-05-19 15:24:58 +0900

    [Passed] 3N + 1 수열 (128ms, 10MB)

---
+++ trail1/3N + 1 수열/3n-plus-1-sequence.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int cnt = 0;
+        while (N > 1) {
+            if (N % 2 == 0) {N /= 2;}
+            else {N = 3 * N + 1;}
+            cnt++;
+        }
+        System.out.println(cnt);
+    }
+}
