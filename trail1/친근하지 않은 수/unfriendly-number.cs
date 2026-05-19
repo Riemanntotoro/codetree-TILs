8a3c1f34302273c69362d2de15fed5209f92da1c
Sanghyeop Lee <geniusky24@outlook.com>
2026-05-19 13:16:18 +0900

    [Passed] 친근하지 않은 수 (67ms, 5MB)

---
+++ trail1/친근하지 않은 수/unfriendly-number.cs
@@ -0,0 +1,14 @@
+using System;
+
+public class Codetree
+{  
+    public static void Main()
+    {
+        int N = int.Parse(Console.ReadLine());
+        int cnt = 0;
+        for (int i=1; i<=N; i++) {
+            if (i % 2 == 0 || i % 3 == 0 || i % 5 == 0) {cnt++;}
+        }
+        Console.WriteLine(N - cnt);
+    }
+}
