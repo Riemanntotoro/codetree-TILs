9123a409150ee5c0858ed1b1ab9d999f16f41934
Sanghyeop Lee <geniusky24@outlook.com>
2026-05-20 09:32:46 +0900

    [Passed] ab 사이에 있는 c (2ms, 0MB)

---
+++ trail1/ab 사이에 있는 c/c-between-a-and-b.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b, c;
+    cin >> a >> b >> c;
+    bool flag = false;
+    for (int i=a; i<=b; i++) {if (i % c == 0) {flag = true; break;} }
+    cout << ((flag == true) ? "YES" : "NO") << endl;
+    return 0;
+}
