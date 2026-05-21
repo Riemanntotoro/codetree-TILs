4916c79d1397916ca128c3d30471f13e9c599cbc
Sanghyeop Lee <geniusky24@outlook.com>
2026-05-21 10:29:47 +0900

    [Passed] 정사각형 별표 출력하기 (69ms, 5MB)

---
+++ trail1/정사각형 별표 출력하기/print-stars-in-square.cs
@@ -0,0 +1,16 @@
+using System;
+
+public class Codetree
+{  
+    public static void Main()
+    {
+        int N = int.Parse(Console.ReadLine());
+        for (int i=0; i<N; i++) {
+            for (int j=0; j<N; j++) {
+                Console.Write("*");
+            }
+        Console.WriteLine();
+        }
+
+    }
+}
