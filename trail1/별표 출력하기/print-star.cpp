e5baeb55f0b624fa1564f8b17d309a7543955af0
Sanghyeop Lee <geniusky24@outlook.com>
2026-06-04 17:50:54 +0900

    [Passed] 별표 출력하기 (2ms, 0MB)

---
+++ trail1/별표 출력하기/print-star.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int N;
+    cin >> N;
+    for (int i=0; i<N; i++) {
+        for (int j=0; j<=i; j++) {cout << "*" << " ";}
+        cout << "\n";
+    }
+    for (int i=N-1; i>0; i--) {
+        for (int j=0; j<i; j++) {cout << "*" << " ";}
+        cout << "\n";
+    }
+    return 0;
+}
