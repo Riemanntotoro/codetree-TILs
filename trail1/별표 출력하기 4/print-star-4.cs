b6c394da66f280e3bd5826068115405d0703146c
Sanghyeop Lee <geniusky24@outlook.com>
2026-06-05 08:54:48 +0900

    [Passed] 별표 출력하기 4 (69ms, 5MB)

---
+++ trail1/별표 출력하기 4/print-star-4.cs
@@ -0,0 +1,17 @@
+using System;
+
+public class Codetree
+{  
+    public static void Main()
+    {
+        int N = int.Parse(Console.ReadLine());
+        for (int i=N; i>=1; i--) {
+            for (int j=0; j<i; j++) {Console.Write("* ");}
+            Console.WriteLine();
+        }
+        for (int i=2; i<=N; i++) {
+            for (int j=0; j<i; j++) {Console.Write("* ");}
+            Console.WriteLine();
+        }
+    }
+}
