bcc3716661a4c5d951c38d51c577e3505bef014c
Sanghyeop Lee <geniusky24@outlook.com>
2026-06-09 14:40:06 +0900

    [Passed] 별표 출력하기 10 (136ms, 10MB)

---
+++ trail1/별표 출력하기 10/print-star-10.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        for (int i=1; i<=2*N; i++) {
+            if (i <= N) { 
+                if (i % 2 == 0) {
+                    for (int j=0; j<(N-i/2+1); j++) {System.out.print("* ");}
+                }
+                else {for (int j=0; j<(i+1)/2; j++) {System.out.print("* ");}}
+            }
+            else {
+                int k = 2*N+1-i;
+                if (k % 2 == 0) {
+                    for (int j=0; j<(N-k/2+1); j++) {System.out.print("* ");}
+                }
+                else {for (int j=0; j<(k+1)/2; j++) {System.out.print("* ");}}
+            }
+            System.out.println();
+        }
+    }
+}
