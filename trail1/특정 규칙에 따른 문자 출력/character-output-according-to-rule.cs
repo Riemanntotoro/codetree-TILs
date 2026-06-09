b3d262cebe2787c269ce120f4b66574b1efb38e5
Sanghyeop Lee <geniusky24@outlook.com>
2026-06-09 12:08:37 +0900

    [Passed] 특정 규칙에 따른 문자 출력 (67ms, 5MB)

---
+++ trail1/특정 규칙에 따른 문자 출력/character-output-according-to-rule.cs
@@ -0,0 +1,19 @@
+using System;
+
+public class Codetree
+{  
+    public static void Main()
+    {
+        int N = int.Parse(Console.ReadLine());
+        for (int i=1; i<=2*N-1; i++) {
+            if (i <= N) {
+                for (int j=0; j<2*(N-i); j++) {Console.Write(" ");}
+                for (int k=0; k<i; k++) {Console.Write("@ ");}
+            }
+            else {
+                for (int k=0; k<2*N-i; k++) {Console.Write("@ ");}
+            }
+            Console.WriteLine();
+        }
+    }
+}
