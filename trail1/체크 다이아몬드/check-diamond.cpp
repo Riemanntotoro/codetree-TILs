4f152cf905797a645419aef02f329df0214f4e23
Sanghyeop Lee <geniusky24@outlook.com>
2026-06-09 13:13:25 +0900

    [Passed] 체크 다이아몬드 (2ms, 0MB)

---
+++ trail1/체크 다이아몬드/check-diamond.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int N;
+    cin >> N;
+    for (int i=1; i<=2*N-1; i++) {
+        if (i<N) {
+            for (int j=0; j<N-i; j++) {cout << " ";}
+            for (int k=0; k<i; k++) {cout << "*" << " ";}
+        }
+        if (i>N) {
+            for (int j=0; j<i-N; j++) {cout << " ";}
+            for (int k=0; k<2*N-i; k++) {cout << "*" << " ";}
+        }
+        if (i==N) {
+            for (int k=0; k<N; k++) {cout << "*" << " ";}
+        }
+        cout << "\n";
+    }
+    return 0;
+}
