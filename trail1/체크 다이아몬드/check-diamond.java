22b3a77b2599036cdf604c96a9e09083a8704ef7
Sanghyeop Lee <geniusky24@outlook.com>
2026-06-09 13:06:12 +0900

    [Passed] 체크 다이아몬드 (137ms, 10MB)

---
+++ trail1/체크 다이아몬드/check-diamond.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        for (int i=1; i<=2*N-1; i++) {
+            if (i<N) {
+                for (int j=0; j<(N-i); j++) {
+                    System.out.print(" ");
+                }
+                for (int k=0; k<i; k++) {
+                    System.out.print("* ");
+                }
+            }
+            else if (i>N) {
+                for (int j=0; j<(i-N); j++) {
+                    System.out.print(" ");
+                }
+                for (int k=0; k<2*N-i; k++) {
+                    System.out.print("* ");
+                }
+            }
+            else {
+                for (int k=0; k<N; k++) {
+                    System.out.print("* ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
