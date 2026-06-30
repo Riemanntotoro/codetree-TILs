41ef7d76edc5eb226f17a615bedbbd6d97513841
Sanghyeop Lee <geniusky24@outlook.com>
2026-06-30 18:36:34 +0900

    [Passed] 열 단위로 반복하여 출력 (83ms, 28MB)

---
+++ trail1/열 단위로 반복하여 출력/print-in-column.cs
@@ -0,0 +1,15 @@
+using System;
+
+public class Codetree
+{  
+    public static void Main()
+    {
+        int N = int.Parse(Console.ReadLine());
+        for (int i=1; i<=N; i++) {
+            for (int j=0; j<N; j++) {
+                Console.Write(i);
+            }
+            Console.WriteLine();
+        }
+    }
+}
