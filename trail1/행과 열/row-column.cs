ecbe9eaefb2cf52e97ece63a166fcf1aa09094cc
Sanghyeop Lee <geniusky24@outlook.com>
2026-07-02 13:47:48 +0900

    [Passed] 행과 열 (65ms, 6MB)

---
+++ trail1/행과 열/row-column.cs
@@ -0,0 +1,16 @@
+using System;
+
+public class Codetree
+{  
+    public static void Main()
+    {
+        String[] input = Console.ReadLine().Split();
+        (int A, int B) = (int.Parse(input[0]), int.Parse(input[1]));
+        for (int i=1; i<=A; i++) {
+            for (int j=1; j<=B; j++) {
+                Console.Write(i*j + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
