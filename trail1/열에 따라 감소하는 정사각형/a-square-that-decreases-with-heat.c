3a2f4cdb389a434cfd640d761c59a6832ddea6c0
Sanghyeop Lee <geniusky24@outlook.com>
2026-07-02 13:32:10 +0900

    [Passed] 열에 따라 감소하는 정사각형 (1ms, 0MB)

---
+++ trail1/열에 따라 감소하는 정사각형/a-square-that-decreases-with-heat.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int N;
+    scanf("%d", &N);
+    for (int i=0; i<N; i++) {
+        for (int j=N; j>0; j--) {
+            printf("%d ", j);
+        }
+        printf("\n");
+    }
+    return 0;
+}
