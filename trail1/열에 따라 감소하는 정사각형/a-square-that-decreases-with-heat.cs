711b5f5da1536e1af560bb1e64f36c1896849974
Sanghyeop Lee <geniusky24@outlook.com>
2026-07-02 13:33:43 +0900

    [Passed] 열에 따라 감소하는 정사각형 (70ms, 7MB)

---
+++ trail1/열에 따라 감소하는 정사각형/a-square-that-decreases-with-heat.cs
@@ -0,0 +1,15 @@
+using System;
+
+public class Codetree
+{  
+    public static void Main()
+    {
+        int N = int.Parse(Console.ReadLine());
+        for (int i=0; i<N; i++) {
+            for (int j=N; j>0; j--) {
+                Console.Write(j + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
