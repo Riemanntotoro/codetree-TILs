ab1bae5099ae018578a9c0c1264eb832fc42a905
Sanghyeop Lee <geniusky24@outlook.com>
2026-07-02 14:20:04 +0900

    [Passed] 좌우반전시킨 정사각형 (1ms, 0MB)

---
+++ trail1/좌우반전시킨 정사각형/left-right-inverted-square.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int N;
+    scanf("%d", &N);
+    for (int i=1; i<=N; i++) {
+        for (int j=N; j>0; j--) {
+            printf("%d ", i*j);
+        }
+        printf("\n");
+    }
+    return 0;
+}
